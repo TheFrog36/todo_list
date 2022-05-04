8195b39f3a58e0e2700e04937f6aaeab823e6690
Frog <artisangiorgio98@gmail.com>
2022-05-04 19:20:27 +0200

    js update

--- model.js
+++ model.js
@@ -1,46 +1,95 @@
 class ToDo {
-    constructor(name, prority = TYPE.basso, tags = []){
-        this.name = name
-        this.prority = prority
-        this._creationDate = new Date().getTime //??
-        this.tags = tags
+    constructor(name, priority = TYPE.basso, tags = []){
+        this.name = name;
+        this.priority = priority;
+        this._creationDate = new Date().getTime();
+        this.tags = tags;
     }
 
     static TYPE = {
         basso: 0,
         medio: 1,
         alto: 2,
-        moltoAlto: 3
+        moltoAlto: 3,
     }
 
-    //name
-    //prority TYPE bassa = 0 media = 1 alta = 2 molto alta = 3  default = basso
-    //creation date (as a number) create a getter defualt = now (sempre default)
-    //tags array di stringhe defaut = 'none'
-    //costruttore
-        //solo il nome è obbligatorio
+    get creationDate(){
+        return new Date(this._creationDate)
+    }
+    
+    toString(){
+        return 'name: ' + this.name + '\n' +
+               'prority: ' + this.priority + '\n' +
+               'creationDate: ' + this.creationDate + '\n' +
+               'tags: ' + this.tags + '\n';
+    }
 }
 
 class MultiToDo extends ToDo{
-    constructor(name, prority, tags, subToDo= []){
-        super(name, prority, tags)
+    constructor(name, priority, tags, subToDo= []){
+        super(name, priority, tags)
         this.subToDo = subToDo
+        this.actual_priority = this.getPriority()
     }
-    //subToDos [ToDoBase1, ToDoBase2, ToDoBase3, ToDoBase4]
-    //getPriority = gives max priority within his subtodos
-    getPrority(){                                                        //New
+    
+    getPriority(){    //Restituisce la priorità più alta tra this.priority e le priorità di subToDo
+        if(this.priority === 3) return 3;
+        const array_highest_priority = this.subToDo.reduce((p,c) => p.priority > c.priority ? p:c)
+        return array_highest_priority.priority > this.priority ? array_highest_priority.priority : this.priority
+    }
 
+    toString(){    //Stampo i dati di MultiToDo e i nomi e priorità di subToDo
+        const to_do_string = super.toString();
+        let my_string = 'actual priority: ' + this.actual_priority + '\n';
+        for (const iterator of this.subToDo) {
+            my_string += 'task name: ' + iterator.name + ' priority: ' + iterator.priority + '\n';
+        }
+        return to_do_string + my_string
+    }
+
+    toStringOP(){    //Stampo tutto
+        const to_do_string = super.toString();
+        return to_do_string + 'actual priority: ' + this.actual_priority + '\n' + this.subToDo.toString();
     }
 }
 
 class ExpiringToDo extends ToDo{
-    constructor(name, prority, tags, deadLine){
-        super(name, prority, tags)
-        this._deadLine = deadLine
+    constructor(name, priority, tags, deadLine){
+        super(name, priority, tags);
+        this._deadLine = deadLine;
+        this.time_left = this.timeLeft();
+        this.actual_priority = this.getPriority();
     }
-    //deadLine default = tomorrow
-    //getPriority = maxPriority between it and max-priority - day left
-    getPrority(){                                                       //new
 
+    get deadLine(){    //imposta la data di scadenza come numero
+        const date = new Date(this._deadLine);
+        return date;
+    }
+    
+    set deadLine(value){    // restituisce la data di scadenza nel formato normale
+        const time = value.getTime();
+        this._deadLine = time;     
+    }
+
+    toString(){    //stampa i vari dati  
+        return super.toString() +
+               'dead line; ' + this.deadLine + '\n' +
+               'actual priority:' + this.actual_priority + '\n';
+    }
+
+    timeLeft(){    //restituisce il tempo rimanente prima della data di scadenza
+        const time_difference = this._deadLine - this._creationDate;
+        const days_left = Math.floor(time_difference /(1000*60*60*24));
+        const hours_left = Math.floor((time_difference % 1000*60*60*24)/(1000*60*60)) ;
+        const minutes_left = Math.floor((time_difference %1000*60*60) / (1000*60));
+        return 'giorni: ' + days_left + '\n' +   
+               'ore: ' + hours_left + '\n' +
+               'minuti: ' + minutes_left;
+    }
+    getPriority(){    //Restituisce la priorità in base ai giorni rimanenti                     
+        const time_difference = this._deadLine - this._creationDate;
+        if(time_difference < 0) return "EXPIRED";
+        const days_left = Math.floor(time_difference /(1000*60*60*24));
+        return days_left < this.priority ? 3-days_left : this.priority;
     }
 }
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const toDo1 = new ExpiringToDo ("bolletta luce", ToDo.TYPE.medio,"pagamento", new Date('2022-05-04'))
-const toDo2 = new ExpiringToDo ("scadenza patente", ToDo.TYPE.basso, 'rinnovo', new Date('2027-05-04'))
-const toDo3 = new ExpiringToDo ("abbonamneto xbox" , toDo.TYPE.alto,'xbox', new Date('2022-05-04'))
-const toDo4 = new ToDo ('latte', ToDo.TYPE.moltoAlto, 'spesa')
+const toDo1 = new ExpiringToDo ("bolletta luce", ToDo.TYPE.medio,"pagamento", new Date('2022-07-04'));
+const toDo2 = new ExpiringToDo ("scadenza patente", ToDo.TYPE.basso, 'rinnovo', new Date('2027-05-04'));
+const toDo3 = new ExpiringToDo ("abbonamento xbox" , ToDo.TYPE.alto,'xbox', new Date('2022-05-07'));
+const toDo4 = new ToDo ('latte', ToDo.TYPE.moltoAlto, 'spesa');
+const toDo5 = new MultiToDo('scadenze', ToDo.TYPE.medio, 'vario', [toDo1, toDo2, toDo3]);
 
-const toDo5 = new MultiToDo('scadenze', ToDo.TYPE.medio, 'vario', [toDo1, toDo2, toDo3])
+
+console.log(toDo1.timeLeft());
+console.log(toDo1.toString());
+console.log(toDo5.toString());
+console.log(toDo5.toStringOP());
+
